02d587bbbe7f5dacd26482380a89617457dc8532
Jacob-048 <0483370@schoolsnet.act.edu.au>
2026-04-29 09:28:48 +1000

    Setup some functions that will be able to read and store data from a .csv file

--- Assignment_3/Form1.cs
+++ Assignment_3/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Assignment_3
 {
@@ -15,11 +16,50 @@ namespace Assignment_3
         public Form1()
         {
             InitializeComponent();
+
+            mainDataView.Columns.Add("ID", "ID");
+            mainDataView.Columns.Add("ProductName", "Name");
+            mainDataView.Columns.Add("ProductBrand", "Brand");
+            mainDataView.Columns.Add("ProductPrice", "Price");
+            mainDataView.Columns.Add("ProductDescription", "Description");
+            mainDataView.Columns.Add("ProductColor", "Colour");
+
+            DataTable dtNew = new DataTable();
+            dtNew = CSVToDataTable("H:/Programming/Product_Catalogue.csv");
+        }
+
+        public DataTable CSVToDataTable(string path)
+        {
+            DataTable csvData = new DataTable();
+
+
+            return csvData;
         }
 
         private void mainDataView_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
 
+        }
+    }
+
+    //struct to handle products
+    public struct Product
+    {
+        int ID;
+        string productName;
+        string productBrand;
+        float price;
+        string description;
+        string color;
+
+        public Product(int newID, string newName, string newBrand, float newPrice, string newDescription, string newColor)
+        {
+            ID = newID;
+            productName = newName;
+            productBrand = newBrand;
+            price = newPrice;
+            description = newDescription;
+            color = newColor;
         }
     }
 }
