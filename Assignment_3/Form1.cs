47565a54f4cf0ab4403fd9ec81f3132180b0b3ed
Jacob-048 <0483370@schoolsnet.act.edu.au>
2026-05-08 12:55:38 +1000

    Moved product into its own class

--- Assignment_3/Form1.cs
+++ Assignment_3/Form1.cs
@@ -32,96 +32,10 @@ namespace Assignment_3
         private void ActivateDataView()
         {
             DataTable dtNew = new DataTable();
-            //dtNew = CSVToDataTable(CSVFilePath);
-            products = CSVToProductList("./saved.csv");
-            dtNew = ProductsToDataTable(products);
+            dtNew = CSVToDataTable(CSVFilePath);
 
             mainDataView.DataSource = dtNew;
             mainDataView.Refresh();
-        }
-
-        public List<Product> CSVToProductList(string path)
-        {
-            List<Product> prod = new List<Product>();
-
-            try
-            {
-                if(path.EndsWith(".csv"))
-                {
-                    using(StreamReader sr = new StreamReader(path))
-                    {
-                        //read the first line to skip the headers
-                        sr.ReadLine();
-
-                        Product newProduct = new Product();
-
-                        while(!sr.EndOfStream)
-                        {
-                            string[] line = sr.ReadLine().Split(',');
-
-                            for(int i = 0; i < line.Length; i++)
-                            {
-                                
-                                switch(i)
-                                {
-                                    //ID
-                                    case 0:
-                                        newProduct.ID = int.Parse(line[i]);
-                                        break;
-                                    //Name
-                                    case 1:
-                                        newProduct.productName = line[i];
-                                        break;
-                                    //Brand
-                                    case 2:
-                                        newProduct.productBrand = line[i];
-                                        break;
-                                    //Price
-                                    case 3:
-                                        newProduct.price = double.Parse(line[i]);
-                                        break;
-                                    //Description
-                                    case 4:
-                                        newProduct.description = line[i];
-                                        break;
-                                    //Color
-                                    case 5:
-                                        newProduct.color = line[i];
-                                        break;
-                                    default:
-                                        break;
-
-                                }
-                            }
-
-                            prod.Add(newProduct);
-                        }
-                    }
-                }
-            }
-            catch
-            {
-
-            }
-
-            return prod;
-        }
-
-        public DataTable ProductsToDataTable(List<Product> prod)
-        {
-            DataTable table = new DataTable();
-            Type structType = typeof(Product);
-
-            FieldInfo[] fields = structType.GetFields();
-
-            for(int i = 0; i < fields.Length; i++)
-            {
-                table.Columns.Add(fields[i].Name);
-            }
-
-            
-
-            return table;
         }
 
         public DataTable CSVToDataTable(string path)
@@ -197,27 +111,6 @@ namespace Assignment_3
             }
 
             return foundPath;  
-        }
-    }
-
-    //struct to handle products
-    public struct Product
-    {
-        public int ID;
-        public string productName;
-        public string productBrand;
-        public double price;
-        public string description;
-        public string color;
-
-        public Product(int newID, string newName, string newBrand, float newPrice, string newDescription, string newColor)
-        {
-            ID = newID;
-            productName = newName;
-            productBrand = newBrand;
-            price = newPrice;
-            description = newDescription;
-            color = newColor;
         }
     }
 }
---
+++ Assignment_3/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_3
+{
+    internal class Product
+    {
+        public int productID { get; set; }
+        public string productName { get; set; }
+        public string productBrand { get; set; }
+        public double productPrice { get; set; }
+        public int productQuantity { get; set; }
+
+        public Product(int id, string name, string brand, float price, int quantity)
+        {
+            productID = id;
+            productName = name;
+            productBrand = brand;
+            productPrice = price;
+            productQuantity = quantity;
+        }
+    }
+}
